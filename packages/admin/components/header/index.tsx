1ef9e3cde60ff4dc842ff598b8fd4dd909c5de6a
Yudu Ban <banyudu@gmail.com>
2020-02-25 20:15:48 +0800

    feat: add admin site

---
+++ packages/admin/components/header/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const Header: FC<any> = (props) => {
+  return <header></header>
+}
+
+export default Header
---
+++ packages/admin/pages/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Head from 'next/head'
+import './index.less'
+import { NextPage } from 'next'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+const App: NextPage = () => {
+  return (
+    <div className='App'>
+      <Head>
+        <title>Yudu</title>
+      </Head>
+      <Header />
+      <article className='App-content'>
+        <img src='/logo.svg' className='App-logo' alt='logo' />
+      </article>
+      <Footer />
+    </div>
+  )
+}
+
+App.getInitialProps = ({ res }) => {
+  // set cachec-control
+  // if (res) {
+  //   res.setHeader('Cache-Control', 'max-age=86400, public')
+  // }
+  return {}
+}
+
+export default App
