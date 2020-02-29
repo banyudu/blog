34cc249b720df911fc2611add488c9641b1e9c63
Yudu Ban <banyudu@gmail.com>
2020-02-29 10:56:27 +0800

    chore: add global secondary index

--- packages/backend/handler.ts
+++ packages/backend/handler.ts
@@ -3,7 +3,7 @@ import './config'
 
 export { getCategories } from './controllers/category'
 export { getTags } from './controllers/tag'
-export { getPosts, getPost, syncPost } from './controllers/post'
+export { getPosts, getPost } from './controllers/post'
 export { getComments, addComment } from './controllers/comment'
 export { getGists, syncGists } from './controllers/gist'
 export { getBlogs } from './controllers/blog'
