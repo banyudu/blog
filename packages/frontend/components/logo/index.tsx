060f6508d49d134fc841545558bc38918a00a096
Yudu Ban <banyudu@gmail.com>
2020-03-01 21:38:26 +0800

    fix(frontend): fix logo image broken link

--- packages/frontend/components/logo/index.tsx
+++ packages/frontend/components/logo/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react'
 import Link from 'next/link'
 import './index.less'
+import logo from './logo.png'
 
 const Logo: FC<any> = (props) => {
   return (
     <Link href='/'>
-      <img className='logo' src='/static/images/logo.png' />
+      <img className='logo' src={logo} />
     </Link>
   )
 }
