03dfe5280af2472e124e64e96633f8d957cde27d
Yudu Ban <banyudu@gmail.com>
2020-06-26 18:00:00 +0800

    feat: modify code block theme

--- packages/frontend/components/codeblock/index.tsx
+++ packages/frontend/components/codeblock/index.tsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { solarizedlight as codeStyle } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+// import { solarizedlight as codeStyle } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import './index.less'
 
 interface CodeBlockProps {
@@ -27,7 +27,7 @@ class CodeBlock extends PureComponent<CodeBlockProps> {
     return (
       <SyntaxHighlighter
         language={language === 'react' ? 'javascript' : language}
-        style={codeStyle}
+        // style={codeStyle}
         showLineNumbers
       >
         {value}
