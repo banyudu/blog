ae6b71bb77c40ce1db935a19696d421c3294c9df
Yudu Ban <banyudu@gmail.com>
2020-10-04 12:19:16 +0800

    fix: open link in new tab

--- packages/frontend/components/markdown/index.tsx
+++ packages/frontend/components/markdown/index.tsx
@@ -8,12 +8,19 @@ interface MarkdownProps {
   className?: string
 }
 
+function LinkRenderer (props) {
+  return <a href={props.href} target='_blank' rel='noopener noreferrer'>{props.children}</a>
+}
+
 const Markdown: FC<MarkdownProps> = (props) => {
   const { source, className } = props
   return (
     <ReactMarkdown
       source={source}
-      renderers={{ code: CodeBlock }}
+      renderers={{
+        code: CodeBlock,
+        link: LinkRenderer
+      }}
       className={`${className} markdown`}
     />
   )
