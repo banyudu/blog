eebc1ea05697f107bea58ea82454be9d94cc85bb
Yudu Ban <banyudu@gmail.com>
2021-01-11 23:12:38 +0800

    fix: add title in follow buttons

--- packages/frontend/components/follow-me/index.tsx
+++ packages/frontend/components/follow-me/index.tsx
@@ -4,8 +4,8 @@ import './index.less'
 const FollowMe = () => (
   <div className='follow-me'>
     <h3>关注我</h3>
-    <a target='_blank' rel='noopener noreferrer' href='https://github.com/banyudu'><img src='https://banyudu.github.io/images/github.png' alt='github' width='64' /></a>
-    <a target='_blank' rel='noopener noreferrer' href='https://twitter.com/yuduban'><img src='https://banyudu.github.io/images/twitter-logo.png' alt='twitter' width='64' /></a>
+    <a target='_blank' title='Github' rel='noopener noreferrer' href='https://github.com/banyudu'><img src='https://banyudu.github.io/images/github.png' alt='github' width='64' /></a>
+    <a target='_blank' title='Twitter' rel='noopener noreferrer' href='https://twitter.com/yuduban'><img src='https://banyudu.github.io/images/twitter-logo.png' alt='twitter' width='64' /></a>
   </div>
 )
 
