f95bd1b84f80dd67f5926c87e60cb7291b7da900
Yudu Ban <banyudu@gmail.com>
2021-03-24 18:40:51 +0800

    refactor: fix deprecation warnings

--- packages/backend/utils/index.ts
+++ packages/backend/utils/index.ts
@@ -85,8 +85,3 @@ rest.interceptors.response.use(res => {
   console.error(err)
   throw err
 })
-
-// 无用代码，为了使lambda发生变化而加的，后面可去掉 https://www.serverless.com/framework/docs/deprecations#default-providerlambdahashingversion
-if (process.env.DEBUG) {
-  console.log('debug')
-}
