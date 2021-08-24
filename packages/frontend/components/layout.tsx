31e8e49fe92506935d887216ba954125006727d2
Yudu Ban <banyudu@gmail.com>
2021-08-24 20:56:50 +0800

    style: update style

--- packages/frontend/components/layout.tsx
+++ packages/frontend/components/layout.tsx
@@ -4,7 +4,7 @@ import Footer from 'components/footer'
 
 const Layout: FC<{ children: JSX.Element | JSX.Element[]}> = ({ children }) => {
   return (
-    <div className='ml-auto mr-auto mt-8 mb-8 px-4 py-8 max-w-2xl text-lg'>
+    <div className='ml-auto mr-auto my-4 px-4 py-8 max-w-2xl text-lg'>
       <Header />
       {children}
       <Footer />
