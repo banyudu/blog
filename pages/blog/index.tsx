76829769c8999975bcb2e54184d6418ea7135897
banyudu <banyudu@gmail.com>
2022-05-04 18:49:08 +0800

    feat: use static props in blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { NextPage } from 'next'
+import { NextPage, GetStaticPaths, GetStaticProps } from 'next'
 import Posts from 'components/posts'
 import Layout from 'components/layout'
 import { usePosts } from 'hooks'
+import { getPosts } from 'services/graph'
+import { Post } from 'types'
 
 
-const Blog: NextPage = () => {
-  const { posts = [], loading } = usePosts()
+const Blog: NextPage<{posts: Post[]}> = ({ posts: staticPosts }) => {
+  const { posts = [] } = usePosts()
   return (
-    <Layout loading={loading}>
-      { loading ? null : <Posts posts={posts} /> }
+    <Layout>
+      <Posts posts={posts ?? staticPosts} />
     </Layout>
   )
 }
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const posts = await getPosts()
+  return {
+    props: {
+      posts
+    },
+  };
+};
 
 export default Blog
